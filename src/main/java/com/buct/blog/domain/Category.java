d56137766ec7bf683307d4635272491c5c7f80e9
qianqianjun <snckfz2017@163.com>
2019-12-27 11:21:50 +0800

    完成首页功能的展示部分的全部代码，链接还没有添加

--- src/main/java/com/buct/blog/domain/Category.java
+++ src/main/java/com/buct/blog/domain/Category.java
@@ -1,13 +1,58 @@
 package com.buct.blog.domain;
 
+import lombok.Data;
+import org.aspectj.lang.annotation.DeclareAnnotation;
+
 /**
  * @author 高谦
  * 分类专栏结构表格
  */
+
+@Data
 public class Category {
     private Integer id;
     private String name;
     private String description;
     private String imgurl;
     private String publishdate;
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPublishdate(String publishdate) {
+        this.publishdate = publishdate;
+    }
+
+    public String getPublishdate() {
+        return publishdate;
+    }
 }
