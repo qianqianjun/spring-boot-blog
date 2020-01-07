7b1b9d32cee9508f8f95f9074b13a38427ec838e
qianqianjun <snckfz2017@163.com>
2020-01-07 16:07:49 +0800

    项目部署设置，成功部署，修改了一些已知问题

--- src/main/java/com/buct/blog/BlogApplication.java
+++ src/main/java/com/buct/blog/BlogApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author  高谦
@@ -12,10 +14,14 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  */
 @SpringBootApplication
 @MapperScan("com.buct.blog.dao")
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
+    }
 }
